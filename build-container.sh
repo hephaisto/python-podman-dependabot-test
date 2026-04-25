77f9f8a22ff1c1dbcbaf4a36be560114d8af28a8
hephaisto <1156478+hephaisto@users.noreply.github.com>
2026-04-25 23:57:57 +0200

    Add cache flags

--- build-container.sh
+++ build-container.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 set -x
+
+cache_flags=$1
+
 scriptdir=$(cd $(dirname $0) && pwd)
 project_name=$(basename "$scriptdir")
 
@@ -8,11 +11,11 @@ echo "### BUILDING WHEEL ###"
 mkdir -p tmp/wheels
 rm -rf tmp/wheels/*
 build_container="build-${project_name}:latest"
-podman build -f build.Containerfile -t ${build_container} .
+podman build ${cache_flags} -f build.Containerfile -t ${build_container} .
 container_id=$(podman create ${build_container})
 podman cp "${container_id}:/out" ./tmp/wheels
 podman rm "${container_id}"
 
 
 echo "### BUILDING RUN-CONTAINER ###"
-podman build -f run.Containerfile --build-arg=build_container=${build_container} -t ${project_name}:local .
+podman build ${cache_flags} -f run.Containerfile --build-arg=build_container=${build_container} -t ${project_name}:local .
