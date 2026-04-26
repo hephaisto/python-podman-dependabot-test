ee60761b185e7c155a6427d601b972ec35533741
hephaisto <1156478+hephaisto@users.noreply.github.com>
2026-04-26 02:30:01 +0200

    split off cmdline

--- src/python_podman_dependabot_test/main.py
+++ src/python_podman_dependabot_test/main.py
@@ -10,21 +10,6 @@ def main_page():
             ui.label(dist.metadata["Name"])
             ui.label(dist.version)
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("hostname", type=str, help="Hostname, IP, 0.0.0.0 or [::]")
-    parser.add_argument("--port", type=int, default=8080, help="Port to listen at")
-    return parser.parse_args()
-
-
-def main():
-    args = parse_args()
-    ui.run(title="dependency test", reload=False, host=args.hostname, port=args.port)
-
-def startup_actions():
-    ui.timer(10.0, lambda: save(data_store))
-    ui.timer(24*3600.0, auto_clean_action)
-
 if __name__ == '__main__':
-    main()
+    ui.run()
 
--- test-ci.sh
+++ test-ci.sh
@@ -5,4 +5,4 @@ set -x
 scriptdir=$(cd $(dirname $0) && pwd)
 project_name=$(basename "$scriptdir")
 
-podman run --mount type=bind,src=${scriptdir},target=/src,ro --entrypoint '["bash", "-c", "python -m pip install pytest-asyncio && python -m pytest /src"]' ${project_name}:local
+podman run --mount type=bind,src=${scriptdir},target=/src,ro --entrypoint '["bash", "-c", "python -m pip install pytest-asyncio && python -m pytest -o cache_dir=/root /src"]' ${project_name}:local
